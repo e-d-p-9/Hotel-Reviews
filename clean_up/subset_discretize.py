68721ab463647e412090c59d561c9174a346117a
e-d-p-9 <140538808+e-d-p-9@users.noreply.github.com>
2024-01-07 11:21:49 -0500

    Add dataset into clean up directory to convert notebook into script

---
+++ clean_up/subset_discretize.py
@@ -0,0 +1,6 @@
+# splits the Cleaned_AG_News_Dataset_Unlabeled 1.xlsx dataset into 
+# 1000 random samples (exclusive) and discretizes into 20/40/60/80/100 percent of full message
+
+
+
+# TODO setup indent guides and rulers
