1f0ba38106be98b30b9a3520159a6551a6b658c7
e-d-p-9 <140538808+e-d-p-9@users.noreply.github.com>
2024-01-07 23:22:26 -0600

    Lint script

--- clean_up/subset_discretize.py
+++ clean_up/subset_discretize.py
@@ -8,24 +8,37 @@ import pandas as pd
 
 # __ Helper Functions __________________________________________________________
 def five_split_df(df):
-    '''Splits the cleaned news data set into 5 equal parts and returns a new dataframe.
+    '''Splits the cleaned news data set into 5 equal parts and returns
+    a new dataframe with the 5 splits (per Description) and the percentage
+    of completeness.
 
-    Arguments: df (the pandas dataframe read in from the xlsx file) using pandas.read_excel
+    Arguments: df (the pandas dataframe read in from the xlsx file)
+    using pandas.read_excel()
     
-    Returns: A new dataframe containing the 5 splits is returned.
+    Returns: A new dataframe containing the 5 splits and the percentage
+    is returned.
     '''
     
-    # initialize new empty dataframe to hold splits
-    # TODO: different headers missing review response and user id
-    split_df = pd.DataFrame(columns=['Article Index', 'Class Label', 'Description', 'Percent of Full'])
+    # the first column of the dataset is unnamed
+    # rename the first column to Article Index for clarity
+    df.rename(columns={'Unnamed: 0': 'Article Index'}, inplace=True)
     
-    i = 1
+    # initialize new empty dataframe to hold the 20 percent successive splits
+    # add an additional column for tracking percentage of the full description
+    split_df = pd.DataFrame(columns=['Article Index', 'Class Index',
+                                     'Description', 'Percent of Full'])
+    
+    # integer for printing output of completion in terms of rows complete
+    # vs the total number of rows
+    row_index = 1
+
     # iterate through the reviews dataframe
     for index in df.index:
 
         # output index for status check
-        print(f"{i} of {len(df)}")
-        i += 1
+        print(f"{row_index} of {len(df)}")
+        # increment to update completion status
+        row_index += 1
 
         # get the current rows description
         description = df.loc[index]['Description']
@@ -34,6 +47,7 @@ def five_split_df(df):
 
         # get number of words in review for calculating split
         num_words = len(description_list)
+
         # split into 20, 40, 60, 80, 100
         twenty = description_list[:int(num_words *0.2)]
         fourty = description_list[:int(num_words *0.4)]
@@ -47,24 +61,30 @@ def five_split_df(df):
         eighty = ' '.join(eighty)
         hundred = description
     
-        # append to split dataframe
-        split_df.loc[len(split_df), split_df.columns] = df.loc[index]['Article Index'], df.loc[index]['Class Index'], twenty, 20
-        split_df.loc[len(split_df), split_df.columns] = df.loc[index]['Article Index'], df.loc[index]['Class Index'], fourty, 40
-        split_df.loc[len(split_df), split_df.columns] = df.loc[index]['Article Index'], df.loc[index]['Class Index'], sixty, 60
-        split_df.loc[len(split_df), split_df.columns] = df.loc[index]['Article Index'], df.loc[index]['Class Index'], eighty, 80
-        split_df.loc[len(split_df), split_df.columns] = df.loc[index]['Article Index'], df.loc[index]['Class Index'], hundred, 100
+        # append to the new split dataframe and add percent of full
+        split_df.loc[len(split_df), split_df.columns] = df.loc[index]
+        ['Article Index'], df.loc[index]['Class Index'], twenty, 20
+        split_df.loc[len(split_df), split_df.columns] = df.loc[index]
+        ['Article Index'], df.loc[index]['Class Index'], fourty, 40
+        split_df.loc[len(split_df), split_df.columns] = df.loc[index]
+        ['Article Index'], df.loc[index]['Class Index'], sixty, 60
+        split_df.loc[len(split_df), split_df.columns] = df.loc[index]
+        ['Article Index'], df.loc[index]['Class Index'], eighty, 80
+        split_df.loc[len(split_df), split_df.columns] = df.loc[index]
+        ['Article Index'], df.loc[index]['Class Index'], hundred, 100
     
     # return the new dataframe containing the 5 splits of the review message
     return split_df
 
 
-
+# __ Main ______________________________________________________________________
 if __name__ == "__main__":
     # read in dataset
     df = pd.read_excel("Cleaned_AG_News_Dataset_Unlabeled 1.xlsx")
 
     # get 1000 samples from dataset
-    df = df.sample(1000)
+    num_samples = 1000
+    df = df.sample(num_samples)
 
     # split and discretize
     df = five_split_df(df)
