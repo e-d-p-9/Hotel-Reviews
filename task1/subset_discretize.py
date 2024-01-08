5df73fbb4c897b288f5c17eb2742a2f076f5e5de
e-d-p-9 <140538808+e-d-p-9@users.noreply.github.com>
2024-01-07 23:35:24 -0600

    update docstring at start of file to reflect column names

--- task1/subset_discretize.py
+++ task1/subset_discretize.py
@@ -1,6 +1,6 @@
 """subset_discretize.py:  splits the Cleaned_AG_News_Dataset_Unlabeled 1.xlsx 
 dataset into 1000 random samples (exclusive) and discretizes into
-20/40/60/80/100 percent of full message"""
+20/40/60/80/100 percent of full description"""
 
 # __ Libraries _________________________________________________________________
 import pandas as pd
