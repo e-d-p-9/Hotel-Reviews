13563fef9276450003a32c55dc37b5582abf3076
e-d-p-9 <140538808+e-d-p-9@users.noreply.github.com>
2024-02-21 12:08:54 -0600

    add random shuffle from Mac Numbers and add notebook to regroup them by article index and add resulting csv files

---
+++ task2/add_percentage_column.py
@@ -0,0 +1,6 @@
+"""add_percentage_column.py:  TODO: finish. creates a column in the 
+Hotel_Sub_1.csv to denote the percentage of the full review for each part"""
+
+# __ Libraries ________________________________________________________________
+import pandas as pd
+
